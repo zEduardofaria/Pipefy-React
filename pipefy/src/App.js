23f623c5747fa5640d75482a94d88a89e9ccc00a
Eduardo Faria <z.eduardofaria@gmail.com>
2019-09-25 17:21:57 -0300

    Header created

---
+++ pipefy/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+
+      <GlobalStyle />
+    </Fragment>
+  );
+}
+
+export default App;
