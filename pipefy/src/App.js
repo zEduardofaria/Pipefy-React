2a3d871991d84731ee23d6d30220e87be4fdb812
Eduardo Faria <z.eduardofaria@gmail.com>
2019-09-26 12:17:44 -0300

    List, card and board styled

--- pipefy/src/App.js
+++ pipefy/src/App.js
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
+import Board from './components/Board';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Board />
 
       <GlobalStyle />
     </Fragment>
